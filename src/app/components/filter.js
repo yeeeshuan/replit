bf3012cbb27f9d431846c6f5479cc9b1569f0fae
Ethan Huang <yehuang2020@gmail.com>
2024-03-28 00:20:35 -0400

    removed slice simulator

--- src/app/components/filter.js
+++ src/app/components/filter.js
@@ -8,6 +8,13 @@ import Image from "next/image";
 class Filter extends Component{
     constructor(props){
         super(props);
+        this.state = {
+            clicked: -1
+        }
+    }
+
+    setClicked(idx){
+        this.setState({clicked:idx}); 
     }
     render(){
     return(
@@ -16,11 +23,32 @@ class Filter extends Component{
                 <input className={styles.searchbarSmall} defaultValue="Search & run commands"/>
             </div>
             <div className={styles.filterOptions}>
-                <button className={styles.filterButton}><p>Language</p></button> 
-                <button className={styles.filterButton}><p>Complexity</p></button> 
-                <button className={styles.filterButton}><p>Time</p></button> 
-                <button className={styles.filterButton}><p>People</p></button> 
+                <button className={styles.filterButton} onClick={()=>this.setClicked(0)}><p>Language</p></button> 
+                <button className={styles.filterButton} onClick={()=>this.setClicked(1)}><p>Complexity</p></button> 
+                <button className={styles.filterButton} onClick={()=>this.setClicked(2)}><p>Time</p></button> 
+                <button className={styles.filterButton} onClick={()=>this.setClicked(3)}><p>People</p></button> 
             </div>
+
+            {this.state.clicked===0 && (
+                <div>
+                    <p>Language</p>
+                </div>
+            )}
+            {this.state.clicked===1 && (
+                <div>
+                    <p>Complexity</p>
+                </div>
+            )}  
+            {this.state.clicked===2 && (
+                <div>
+                    <p>Time</p>
+                </div>
+            )}  
+            {this.state.clicked===3 && (
+                <div>
+                    <p>People</p>
+                </div>
+            )}  
         </div>
     )
     }
